97b837eb64e559042fbeaf8c2325cd92e572c3b2
Caio <caiohenriquecosta04@gmail.com>
2024-01-14 21:43:50 -0300

    Criação e Estilização do banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="main-container">
-    <h1>HOME</h1>
+  <div>
+    <Banner />
+    <p>banner</p>
+    <div class="main-container">
+      <h1>Monte o seu burger:</h1>
+      <p>Formulário</p>
+    </div>
   </div>
 </template>
 
 <script>
+  import Banner from "../components/Banner.vue";
 
+  export default {
+    name: 'HomeView',
+    components: {
+      Banner
+    }
+  }
 </script>
